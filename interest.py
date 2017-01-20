021f384475c23c3c9bbe49b801551e06309777fe
Max Compton <maxcct@cantab.net>
2017-01-19 19:08:27 -0600

    Update interest.py

--- interest.py
+++ interest.py
@@ -1,13 +1,19 @@
+balance = 10000
+annualInterestRate = 0.2
+
 def min_payment(balance, annualInterestRate):
     modified_balance = balance
     mInterest = annualInterestRate / 12
     mPmin = balance / 12
     mPmax = (balance * ((1 + mInterest)**12))/12
     mP = (mPmin + mPmax) / 2
+    tries = 1
     for m in range(1, 13):
         modified_balance -= mP
         modified_balance += (mInterest * modified_balance)
     while abs(modified_balance) > 0.01:
+        print(str(tries))
+        tries += 1
         if modified_balance < 0:
             mP = (mP + mPmin) / 2
             modified_balance = balance
@@ -17,4 +23,6 @@ def min_payment(balance, annualInterestRate):
         for m in range(1, 13):
             modified_balance -= mP
             modified_balance += (mInterest * modified_balance)
+            if tries > 3000000 and m == 12:
+                return 'Balance: ' + str(modified_balance) + '; Lowest Payment: ' + str(mP) + '. mPmin: ' + str(mPmin) + '; mPmax: ' + str(mPmax) + '; ' + str((mP + mPmin) / 2)
     return 'Balance: ' + str(modified_balance) + '; Lowest Payment: ' + str(mP)
